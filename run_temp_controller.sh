d1bdc66b193f03cac50245f86899827f21144737
d4daveshep <david@daveshep.net.nz>
2018-06-01 22:33:29 +1200

    initial commit

---
+++ run_temp_controller.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+DIR=/home/david/development/fermenter-temp-controller
+SCRIPT=tempcontroller.py
+CONFIG_FILE=config.txt
+
+# run the above script if it's not running
+pgrep -f $SCRIPT || nohup python3 $DIR/$SCRIPT $DIR/$CONFIG_FILE
+
+
---
+++ tempcontroller.py
@@ -0,0 +1,127 @@
+# simple program to read from Arduino serial port
+
+import serial
+import json
+import glob
+from datetime import datetime
+import time
+import configparser
+import elasticsearch
+from elasticsearch import Elasticsearch
+import sys
+import argparse
+from pathlib import Path
+import logging
+from pytz import timezone
+
+# use a local Elasticsearch instance
+ES_HOST = "192.168.1.55"
+
+# get NZ timezone so we can localise the timestamps and deal with NZST/NZDT
+nztz = timezone("NZ")
+
+# set up the logger
+logging.basicConfig(
+#    filename="tempcontroller.log",
+    level=logging.DEBUG,
+    format="%(levelname)s: %(asctime)s: %(message)s")
+
+### TEST CODE
+###
+
+
+# set up the command line parser
+parser = argparse.ArgumentParser()
+
+# define the argument to set the config file
+parser.add_argument(
+    "config_file", help="specify the full location of the config file")
+
+# parse the arguments and check the file exists
+args = parser.parse_args()
+
+if args.config_file:
+    config_file = args.config_file
+    logging.info("Using config file '%s'", config_file)
+
+    if not Path(config_file).exists():
+        logging.critical("Config file '%s' not found", config_file)
+        raise SystemExit("*** ERROR*** config file not found")
+
+# find the serial port
+ttylist = glob.glob("/dev/ttyACM*")
+if len(ttylist) == 0:
+    msg = "No Arduino devices found at /dev/ttyACM*"
+    logging.critical(msg)
+    raise SystemExit("*** ERROR *** " + msg)
+
+if len(ttylist) > 1:
+    msg = "WARNING: Multiple Arduino devices found at /dev/ttyACM*, using first device"
+    logging.warning(msg)
+
+tty = ttylist[0]
+logging.info("Using Arduino device at %s", tty)
+
+# open the serial port
+try:
+    ser = serial.Serial(tty, 9600)
+except SerialException:
+    logging.error("Couldn't open serial port at %s", tty)
+    raise SystemTxit("*** ERROR *** Couldn't open serial port")
+
+# read the target temp from config file
+config = configparser.ConfigParser()
+config.read(config_file)
+new_target = config["temperature"]["TargetTemp"]
+logger.info("Target temperature being set to %s", str(new_target))
+
+# sleep for 30 secs to allow arduino to reboot after serial port open
+time.sleep(30)  
+
+new_target_str = '<' + str(new_target) + '>'
+
+# write target temp to serial port
+try:
+    ser.write(new_target_str.encode())
+except SerialException:
+    logger.warn("Couldn't write target temp to serial port")
+
+while True:
+    line = ser.readline()  # read serial line as bytes
+
+    # convert serial line to string and load to JSON sequence
+    data = json.loads(line.decode("utf-8"))
+
+    # get formatted localised timestamp
+    stamp = nztz.localize(datetime.now()).strftime("%Y-%m-%dT%H:%M:%S%z") 
+    logging.debug("timestamp is %s", stamp)
+
+    data["timestamp"] = stamp
+#    data['brewid'] = '12-AAA-02'
+    data["brewid"] = '99-TEST-99'
+
+    target = data["target"]
+    
+    # check if we need to update the target temp
+    if target != new_target:
+        new_target_str = '<' + str(new_target) + '>'
+        ser.write(new_target_str.encode())
+        logger.info("Updated target temp to %s", str(new_target_str))
+
+    doc = data
+    logger.info("Indexing %s", json.dumps(doc))
+
+    es = Elasticsearch(
+        hosts=[{'host': ES_HOST, 'port': 9200}],
+        #       use_ssl=True,
+        #       verify_certs=True,
+        connection_class=elasticsearch.connection.RequestsHttpConnection
+    )
+
+    # index the doc to elastic
+    res = es.index(
+        index="brew-temp",
+        doc_type="temp-reading",
+        body=doc
+    )
+    logger.info("Result is %s", json.dumps(res))
