6c55f98ba76c68d9e31b0a249b0113d5f2ab3d72
d4daveshep <david@daveshep.net.nz>
2021-07-05 13:22:02 +1200

    testing influxdb dataframes

--- test_influxdb.py
+++ test_influxdb.py
@@ -88,6 +88,12 @@ def do_dataframes(host="localhost", port=8086):
     print("dataframe is...")
     print(df)
 
+    df.index.tz_localize(timezone.utc)
+    df.index.dt.tz_convert('America/New_York')
+
+    print("dataframe is...")
+    print(df)
+
 
     #
     # df['time'] = df['time'].to_timestamp()
