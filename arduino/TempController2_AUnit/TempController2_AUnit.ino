7977d722cda34e5fe44cfbd0fd796acbd2446512
d4daveshep <david@debian-dev.daveshep.net>
2021-10-24 14:58:58 +1300

    Refactoring before ambient logic gets added

--- arduino/TempController2_AUnit/TempController2_AUnit.ino
+++ arduino/TempController2_AUnit/TempController2_AUnit.ino
@@ -11,6 +11,9 @@ class FermentationProfile {
     
   public:
 
+    FermentationProfile() {
+    }
+    
     FermentationProfile(String name, double temp, double range){
       this->name = name;
 
@@ -39,53 +42,63 @@ class FermentationProfile {
 };
 
 enum Action { ERROR, HEAT, COOL, REST };
-enum ActionStop { ERROR, RANGE_MAX, RANGE_MIN, IN_RANGE };
+enum ActionStop { RANGE_MAX, RANGE_MIN, IN_RANGE };
 
 class ControllerActionRules {
+
+  private:
+  FermentationProfile profile;
+
+  public:
+  ControllerActionRules() {
+  }
+  ControllerActionRules(FermentationProfile fp) {
+    this->profile = fp;
+  }
+  
+  Action getAction(double actualTemp) {
+
+    double target = profile.getFermentationTemp();
+    double range = profile.getTemperatureRange();
+    Action action = ERROR;  // set error if we can't make a decision
+    //Serial.print(actualTemp);
+    //Serial.print("\n");
+  
+    // check failsafe is not breached
+    if( actualTemp > (target + range*2) ) {
+      action = COOL;
+      return action;
+    }
+    if (actualTemp < (target - range*2) ) {
+      action = HEAT;
+      return action;
+    }
+    
+    // temp above range
+    if( actualTemp > (target + range )) {
+      action = COOL;
+      return action;
+    }
+  
+    // temp below range
+    if( actualTemp < target - range ) {
+      action = HEAT;
+      return action;
+    }
+    
+    // temp within range
+    if( actualTemp >= (target - range) && actualTemp <= (target + range) ) {
+      action = REST;
+      return action;
+    }
+    
+    return action;
+    
+  }
+  
   
 };
   
-
-Action getControllerDecision(FermentationProfile fp, double actualTemp) {
-
-  double target = fp.getFermentationTemp();
-  double range = fp.getTemperatureRange();
-  Action decision = ERROR;  // set error if we can't make a decision
-  //Serial.print(actualTemp);
-  //Serial.print("\n");
-
-  // check failsafe is not breached
-  if( actualTemp > (target + range*2) ) {
-    decision = COOL;
-    return decision;
-  }
-  if (actualTemp < (target - range*2) ) {
-    decision = HEAT;
-    return decision;
-  }
-  
-
-  // test above range
-  if( actualTemp > (target + range )) {
-    decision = COOL;
-    return decision;
-  }
-
-  // test below range
-  if( actualTemp < target - range ) {
-    decision = HEAT;
-    return decision;
-  }
-  
-  // test within range
-  if( actualTemp >= (target - range) && actualTemp <= (target + range) ) {
-    decision = REST;
-    return decision;
-  }
-  
-  return decision;
-  
-}
 
 // Test the decision making logic
 
@@ -94,7 +107,7 @@ Action getControllerDecision(FermentationProfile fp, double actualTemp) {
 
   /* 
    *  When ambient temp is "well below" target range then don't need much active cooling (except failsafe)
-   *  
+   *  controller.
    */
 //   
 //  String name = "TestBeer_1";
@@ -102,14 +115,14 @@ Action getControllerDecision(FermentationProfile fp, double actualTemp) {
 //  double range = 0.5;
 //  FermentationProfile fp1(name, target, range);
 //
-//  // When temp AND ambient are below target range then heat to top of target range
+//  // When temp AND ambient are below targetint a = 1/0; range then heat to top of target range
 //  double actual = target - range*1.1;
 //  double ambient = actual - 1.1;
 //
-//  Action decision = getControllerDecision(fp1, actual);  // just over range - should cool
+//  Action action = getAction(fp1, actual);  // just over range - should cool
 //  ActionStop heatStop = getStopStrategy(fp1, actual, ambient)
 //
-//  assertEqual(decision, HEAT); // we should be heating
+//  assertEqual(action, HEAT); // we should be heating
 //  assertEqual(heatStop, RANGE_MAX)
 //  
   //assertTrue(false); // deliberately fail this test 
@@ -121,19 +134,20 @@ test(TempRangeExceeded) {
   double temp = 18.0;
   double range = 0.5;
   FermentationProfile fp1(name, temp, range);
+  ControllerActionRules controller(fp1);
 
-  Action decision;
-  decision = getControllerDecision(fp1, temp + range*1.1);  // just over range - should cool
-  assertEqual(decision, COOL);
+  Action action;
+  action = controller.getAction(temp + range*1.1);  // just over range - should cool
+  assertEqual(action, COOL);
 
-  decision = getControllerDecision(fp1, temp - range*1.1);  // just under range - should heat
-  assertEqual(decision, HEAT);
+  action = controller.getAction(temp - range*1.1);  // just under range - should heat
+  assertEqual(action, HEAT);
 
-  decision = getControllerDecision(fp1, temp + range*0.9);  // just under top end of range. should rest
-  assertEqual(decision, REST);
+  action = controller.getAction(temp + range*0.9);  // just under top end of range. should rest
+  assertEqual(action, REST);
   
-  decision = getControllerDecision(fp1, temp - range*0.9);  // just above bottom end of range. should rest
-  assertEqual(decision, REST);
+  action = controller.getAction(temp - range*0.9);  // just above bottom end of range. should rest
+  assertEqual(action, REST);
   
 }
 
@@ -142,13 +156,14 @@ test(FailsafeExceeded) {
   double temp = 18.0;
   double range = 0.5;
   FermentationProfile fp1(name, temp, range);
+  ControllerActionRules controller(fp1);
 
-  Action decision;
-  decision = getControllerDecision(fp1, temp+range*3);  // way above range so cool
-  assertEqual(decision, COOL);
+  Action action;
+  action = controller.getAction(temp+range*3);  // way above range so cool
+  assertEqual(action, COOL);
 
-  decision = getControllerDecision(fp1, temp-range*3);  // way below range so heat
-  assertEqual(decision, HEAT);
+  action = controller.getAction(temp-range*3);  // way below range so heat
+  assertEqual(action, HEAT);
 
 }
 
@@ -161,7 +176,9 @@ test(fermentation_profile_get_set) {
   FermentationProfile fp1(name, temp, range);
   assertTrue(fp1.isValid());
   assertEqual(fp1.getName(), name); // not really needed
-  assertEqual(fp1.getFermentationTemp(), temp);
+  assertEqual(fp1.gFermentationProfile profile
+
+etFermentationTemp(), temp);
   assertEqual(fp1.getTemperatureRange(), range);
        FermentationProfile fp1("TestBeer_1", 18.0, 0.5);
 
