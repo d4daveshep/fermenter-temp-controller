827eb90485e5adb4c5453a7c2628def9e83feefc
d4daveshep <david@debian-dev.daveshep.net>
2021-10-31 17:27:07 +1300

    removed FermentationProfile class and merged with ControllerActionRules

--- arduino/TempController2_AUnit/TempController2_AUnit.ino
+++ arduino/TempController2_AUnit/TempController2_AUnit.ino
@@ -6,7 +6,6 @@
 #include <AUnit.h>
 #endif
 
-#include "FermentationProfile.h"
 #include "ControllerActionRules.h"
 
 
@@ -14,17 +13,38 @@
 // setup() and loop()
 //----------------------------------------------------------------------------
 
-void setup() {
-  delay(1000); // wait for stability on some boards to prevent garbage Serial
-  Serial.begin(115200); // ESP8266 default of 74880 not supported on Linux
-  while(!Serial); // for the Arduino Leonardo/Micro only
+/*
+* Test real scenario
+*/
 
-  Serial.print("\n");
+double defaultTargetTemp = 6.0;
+double defaultRange = 0.3; // i.e. +/- either side of target
+ControllerActionRules controller(defaultTargetTemp, defaultRange);
+
+
+test(UpdatedTargetTempIsSaved) {
+	double newTargetTemp = 7.0;
+	controller.setTargetTemp(newTargetTemp);
+	
+	assertEqual(controller.getTargetTemp(), newTargetTemp );
+	
+}
+
+void setup() {
+	delay(1000); // wait for stability on some boards to prevent garbage Serial
+	Serial.begin(115200); // ESP8266 default of 74880 not supported on Linux
+	while(!Serial); // for the Arduino Leonardo/Micro only
+
+	Serial.print("\n");
 
 }
 
 void loop() {
 #ifdef _DO_UNIT_TESTING
-  aunit::TestRunner::run();
+	
+	
+	
+	aunit::TestRunner::run();
+
 #endif
 }
