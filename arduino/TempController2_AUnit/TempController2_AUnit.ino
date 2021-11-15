db349e38c5c0e44050ac74e9b7c678e2e3a29f81
d4daveshep <david@daveshep.net.nz>
2021-11-15 15:27:52 +1300

    refactored TemperatureReadings class into separate .h and .cpp

--- arduino/TempController2_AUnit/TempController2_AUnit.ino
+++ arduino/TempController2_AUnit/TempController2_AUnit.ino
@@ -7,201 +7,8 @@
 #endif
 
 #include "ControllerActionRules.h"
+#include "TemperatureReadings.h"
 
-
-//----------------------------------------------------------------------------
-// setup() and loop()
-//----------------------------------------------------------------------------
-
-
-class TemperatureReadings {
-
-private:
-    int numberOfReadingsUsedForAverage;
-    double averageTemperature;
-    double latestTemperatureReading;
-    unsigned long countNumberOfReadings;
-    double minimumTemperatureReading;
-    double maximumTemperatureReading;
-    
-public:
-    TemperatureReadings(int numberOfReadingsUsedForAverage) {
-        setNumberOfReadingsUsedForAverage(numberOfReadingsUsedForAverage);
-        clear();
-    }
-    void clear() {
-        this->averageTemperature = 0.0;
-        this->latestTemperatureReading = 0.0;
-        this->countNumberOfReadings = 0;
-        this->minimumTemperatureReading = 1000.0;
-        this->maximumTemperatureReading = -1000.0;
-    }
-    int getNumberOfReadingsUsedForAverage() {
-        return this->numberOfReadingsUsedForAverage;
-    }
-    void setNumberOfReadingsUsedForAverage(int numberOfReadingsUsedForAverage) {
-        this->numberOfReadingsUsedForAverage = numberOfReadingsUsedForAverage;
-    }
-    double getCurrentAverageTemperature() {
-        return this->averageTemperature;
-    }
-    double getLatestTemperatureReading() {
-        return this->latestTemperatureReading;
-    }
-    unsigned long getCountOfTemperatureReadings() {
-        return this->countNumberOfReadings;
-    }
-    double getMinimumTemperature() {
-        return this->minimumTemperatureReading;
-    }
-    double getMaximumTemperature() {
-        return this->maximumTemperatureReading;
-    }
-    
-    /*
-     * Calculate exponential moving average (EMA)
-     * new average = (old average * (n-1) + new value) / n
-     */
-    void updateAverageTemperatureWithNewValue(double newTemperatureReading) {
-        this->latestTemperatureReading = newTemperatureReading;
-        double newAverage = ( averageTemperature * (numberOfReadingsUsedForAverage-1) + newTemperatureReading ) / numberOfReadingsUsedForAverage;
-        this->averageTemperature = newAverage;
-        this->countNumberOfReadings++;
-        if( newTemperatureReading < this->minimumTemperatureReading ) {
-            this->minimumTemperatureReading = newTemperatureReading;
-        }
-        if( newTemperatureReading > this->maximumTemperatureReading ) {
-            this->maximumTemperatureReading = newTemperatureReading;
-        }
-    }
-    
-    double generateRandomTemperature(int min, int max) {
-        double temp = random( min, max ) + random( 0, 100 ) / 100.0;
-//         Serial.print("random temp = ");
-//         Serial.print(temp);
-//         Serial.print("\n");
-        return temp;
-    }
-    
-    double randomTempMovement() {
-        return random(-10,11) / 100.0;
-    }
-
-};
-
-
-TemperatureReadings temperatureReadings(10);
-
-class TemperatureReadingsTest: public aunit::TestOnce {
-
-
-protected:
-    
-    
-    void setup() override {
-        TestOnce::setup();
-        
-        // set up stuff here
-        Serial.print("Running setup...\n");
-        temperatureReadings.clear(); // just to be safe, zero the averages
-    }
-    
-    void teardown() override {
-        // tear down stuff here
-        temperatureReadings.clear();
-        TestOnce::teardown();
-    }
-    
-    void generateRandomReadings(int numberToGenerate) {
-        for( int i=0; i<numberToGenerate; i++ ) {
-            temperatureReadings.updateAverageTemperatureWithNewValue(temperatureReadings.generateRandomTemperature(15,25));
-        }
-    }
-        
-    
-};
-
-testF(TemperatureReadingsTest, MinAndMaxTemperatures) {
-    
-    assertEqual(1000.0, temperatureReadings.getMinimumTemperature());
-    assertEqual(-1000.0, temperatureReadings.getMaximumTemperature());
-
-    randomSeed(1); randomSeed(0);
-    
-    generateRandomReadings(10);
-    
-    assertEqual(15.12, temperatureReadings.getMinimumTemperature());
-    assertEqual(22.49, temperatureReadings.getMaximumTemperature());
-    
-}
-
-/*
-test(RandomNextTemp) {
-    randomSeed(0); // setup the random number generator to a predictable sequence
-    TemperatureReadings temperatureReadings(10);
-    double currentTemp = 20.0;
-
-    for(int i=0; i<10; i++ ) {
-        currentTemp = currentTemp + temperatureReadings.randomTempMovement();
-        Serial.print( currentTemp );
-        Serial.print("\n");
-    }
-    
-    assertTrue(false);
-    
-}
-*/
-
-testF(TemperatureReadingsTest, EMA) {
-   
-    randomSeed(1); randomSeed(0); 
-    generateRandomReadings(10);
-    assertEqual((unsigned long)10, (unsigned long)(temperatureReadings.getCountOfTemperatureReadings()));
-    assertEqual(12.14163,  temperatureReadings.getCurrentAverageTemperature());
-    
-}   
-
-test(ExponentialMovingAverageOfTemperatureReadings) {
-    randomSeed(0); // setup the random number generator to a predictable sequence
-    //TemperatureReadings temperatureReadings(10);
-    temperatureReadings.clear();
-    //int numberOfReadingsUsedForAverage = temperatureReadings.getNumberOfReadingsUsedForAverage();
-    
-    // first 10 values are 22.49, 18.58, 15.72, 19.78, 18.09, 15.65, 17.42, 22.03, 22.29, 15.12, 
-    // true average is 18.717, exponential moving average is 12.14163 
-    
-    // average starts at 0.0
-    assertEqual(0.0, temperatureReadings.getCurrentAverageTemperature());
-    
-    for( int i=0; i<10; i++ ) {
-        temperatureReadings.updateAverageTemperatureWithNewValue(temperatureReadings.generateRandomTemperature(15,25));
-    }
-    assertEqual(12.14163,  temperatureReadings.getCurrentAverageTemperature());
-    
-    /*
-    // add first value 22.49
-    temperatureReadings.updateAverageTemperatureWithNewValue(22.49);
-    assertEqual( 22.49/10, temperatureReadings.getLatestAverageTemperature()); // 2.249
-    
-    // add next value 18.58
-    temperatureReadings.updateAverageTemperatureWithNewValue(18.58);
-    assertNear( (2.249*9.0+18.58)/10, temperatureReadings.getLatestAverageTemperature(), 0.005); // 3.8821
-    
-    // add next value 15.72
-    temperatureReadings.updateAverageTemperatureWithNewValue(15.72);
-    assertNear( (3.8821*9.0+15.72)/10, temperatureReadings.getLatestAverageTemperature(), 0.005); // 5.065589
-    */
-      
-}
-
-test(UpdateNumberOfTemperatureReadingsUsedForAverage) {
-
-    TemperatureReadings temperatureReadings(10);
-    
-    assertEqual(10, temperatureReadings.getNumberOfReadingsUsedForAverage());
-    temperatureReadings.setNumberOfReadingsUsedForAverage(20);
-    assertEqual(20, temperatureReadings.getNumberOfReadingsUsedForAverage());
-}
 
 
 test(UpdatedTargetTempIsSaved) {
@@ -215,6 +22,13 @@ test(UpdatedTargetTempIsSaved) {
 	assertEqual(controller.getTargetTemp(), newTargetTemp );
 	
 }
+
+//----------------------------------------------------------------------------
+// setup() and loop()
+//----------------------------------------------------------------------------
+
+
+
 
 void setup() {
 	delay(1000); // wait for stability on some boards to prevent garbage Serial
