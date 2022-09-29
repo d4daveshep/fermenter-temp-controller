4c7076cf6cc6aa7aa5438f05b1d1d81997c79f29
d4daveshep <david@daveshep.net.nz>
2022-09-29 17:35:35 +1300

    updated requirements.txt added first mock_serial test

---
+++ python/serial_test.py
@@ -0,0 +1,29 @@
+# from mock_serial import MockSerial
+from serial import Serial
+# import pytest
+
+import logging, sys
+
+logging.basicConfig(
+  stream=sys.stdout,
+  level=logging.DEBUG,
+  format="%(levelname)s - %(message)s"
+)
+
+def test_example(mock_serial):
+
+    serial = Serial(mock_serial.port)  # open a mocked serial port
+
+    stub = mock_serial.stub(
+        receive_bytes=b'123',
+        send_bytes=b'456'
+    )
+
+    serial.write(b'123')
+    assert serial.read(3) == b'456'
+
+    assert stub.called
+    assert stub.calls == 1
+
+    serial.close()
+
