c01b2969cc9d2ebc84663f0ed3c4a2f33ff4ced0
d4daveshep <david@daveshep.net.nz>
2022-09-29 20:54:44 +1300

    testing serial mocking object

--- python/serial_test.py
+++ python/serial_test.py
@@ -14,11 +14,14 @@ def test_example(mock_serial):
 
     serial = Serial(mock_serial.port)  # open a mocked serial port
 
+    # create a stub ojbect that we can use multiple timees,  the
     stub = mock_serial.stub(
         receive_bytes=b'123',
         send_bytes=b'456'
     )
 
+    serial.write(b'123')
+    assert serial.read(3) == b'456'
     serial.write(b'123')
     assert serial.read(3) == b'456'
 
