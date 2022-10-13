58fd61ca2b04ee3bb75abcbdb4f17c810772aea2
d4daveshep <david@daveshep.net.nz>
2022-10-13 21:22:09 +1300

    added script for building and running tests in docker container

---
+++ python/build_run_docker_for_testing.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+IMAGE_NAME="test_fermenter_controller"
+
+#To build docker image
+docker build -t ${IMAGE_NAME} .
+
+#To run docker container in host network mode
+docker run --network="host" ${IMAGE_NAME}
