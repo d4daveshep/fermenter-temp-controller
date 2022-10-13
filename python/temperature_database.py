4e7cd959902d645de986a7bc0eb8caa6c5469b57
d4daveshep <david@daveshep.net.nz>
2022-10-13 18:08:30 +1300

    implemented method to check influxdb server available

--- python/temperature_database.py
+++ python/temperature_database.py
@@ -1,26 +1,23 @@
 import configparser
 
+import requests
+
 from config import ControllerConfig
 
 
 class TemperatureDatabase:
     def __init__(self, config: ControllerConfig):
-        pass
+        self.__config = config
 
     def is_server_available(self):
         # run equivalant of curl -sL -I localhost:8086/ping?wait_for_leader=30s
+        url = self.__config.influxdb_url + "/ping"
+        params = {'wait_for_leader': "30s"}
 
-        # URL = "http://localhost:8086/ping"
-        # PARAMS = {'wait_for_leader': "30s"}
-        #
         # try:
-        #     response = requests.get(url = URL, params = PARAMS)
-        #     assert response.status_code == 204
-        #     assert response.headers["X-Influxdb-Build"] == "OSS"
-        #     assert response.headers["X-Influxdb-Version"] >= "v2.4"
-        # except requests.ConnectionError as err:
-        #     assert False, err
+        response = requests.get(url=url, params=params)
+        return response.status_code == 204 and \
+               response.headers["X-Influxdb-Build"] == "OSS" and \
+               response.headers["X-Influxdb-Version"] >= "v2.4"
         # except Exception as exec:
-        #     assert False, exec
-
-        return False
+        #     return False
