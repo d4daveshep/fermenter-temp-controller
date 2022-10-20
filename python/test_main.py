915df9a97ba40c1ed5dd1002c28104a907df22fd
d4daveshep <david@daveshep.net.nz>
2022-10-20 15:31:30 +1300

    started adding async tests

--- python/test_main.py
+++ python/test_main.py
@@ -1,7 +1,21 @@
 import random
 from time import sleep
-
+import pytest_asyncio
+import pytest
 import main
+import asyncio
+
+
+@pytest.mark.asyncio
+async def test_write_async_serial_string(event_loop):
+    serial_port_reader, serial_port_writer = event_loop.run_until_complete(main.open_serial_connection())
+
+    target_temp = random.randrange(10, 30)
+
+    await main.write_float_to_serial_port(serial_port_writer, target_temp)
+
+    what = False
+    assert what
 
 
 def test_send_and_receive_target_temp_to_serial():
