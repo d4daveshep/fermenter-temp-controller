563363207af1a73e905e92f8673c36bd53cc0898
d4daveshep <david@daveshep.net.nz>
2022-10-22 10:59:01 +1300

    commented out non async serial code

--- python/main.py
+++ python/main.py
@@ -19,38 +19,38 @@ from serial import SerialException, Serial
 #             pass
 #
 
-def write_float_to_serial(serial_port: Serial, num: float):
-    string_to_write = '<' + str(num) + '>'
-    serial_port.write(string_to_write.encode())
-
-
-def read_json_from_serial(serial_port: Serial) -> dict:
-    line = serial_port.readline()
-    return json.loads(line.decode("utf-8"))
-
-
-def get_serial_port() -> Serial:
-    # find the serial port
-    ttylist = glob.glob("/dev/ttyACM*")
-    if len(ttylist) == 0:
-        msg = "No Arduino devices found at /dev/ttyACM*"
-        raise SystemExit("*** ERROR *** " + msg)
-
-    if len(ttylist) > 1:
-        msg = "WARNING: Multiple Arduino devices found at /dev/ttyACM*, using first device"
-
-    tty = ttylist[0]
-
-    # open the serial port
-    try:
-        port = serial.Serial(tty, 115200)
-    except SerialException:
-        raise SystemExit("*** ERROR *** Couldn't open serial port")
-
-    # sleep for 30 secs to allow arduino to reboot after serial port open
-    # time.sleep(30)
-
-    return port
+# def write_float_to_serial(serial_port: Serial, num: float):
+#     string_to_write = '<' + str(num) + '>'
+#     serial_port.write(string_to_write.encode())
+#
+#
+# def read_json_from_serial(serial_port: Serial) -> dict:
+#     line = serial_port.readline()
+#     return json.loads(line.decode("utf-8"))
+#
+#
+# def get_serial_port() -> Serial:
+#     # find the serial port
+#     ttylist = glob.glob("/dev/ttyACM*")
+#     if len(ttylist) == 0:
+#         msg = "No Arduino devices found at /dev/ttyACM*"
+#         raise SystemExit("*** ERROR *** " + msg)
+#
+#     if len(ttylist) > 1:
+#         msg = "WARNING: Multiple Arduino devices found at /dev/ttyACM*, using first device"
+#
+#     tty = ttylist[0]
+#
+#     # open the serial port
+#     try:
+#         port = serial.Serial(tty, 115200)
+#     except SerialException:
+#         raise SystemExit("*** ERROR *** Couldn't open serial port")
+#
+#     # sleep for 30 secs to allow arduino to reboot after serial port open
+#     # time.sleep(30)
+#
+#     return port
 
 
 async def open_serial_connection(port_url: str) -> (StreamReader, StreamWriter):
