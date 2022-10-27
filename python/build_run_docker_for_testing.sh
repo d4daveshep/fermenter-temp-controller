4410c1dd815be02bb8b0e0357c1197c8deb3352e
d4daveshep <david@daveshep.net.nz>
2022-10-28 07:43:19 +1300

    prep docker files for fastapi deployment

--- python/build_run_docker_for_testing.sh
+++ python/build_run_docker_for_testing.sh
@@ -3,7 +3,7 @@
 IMAGE_NAME="test_fermenter_controller"
 
 #To build docker image
-docker build -t ${IMAGE_NAME} .
+docker build -f ./Dockerfile.pytests -t ${IMAGE_NAME} .
 
 #To run docker container in host network mode
 docker run --privileged --network="host" ${IMAGE_NAME}
