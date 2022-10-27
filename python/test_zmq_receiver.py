63472227454bea62c3baf67761c9afff52726fb6
d4daveshep <david@daveshep.net.nz>
2022-10-27 15:00:39 +1300

    building out tests and zm

--- python/test_zmq_receiver.py
+++ python/test_zmq_receiver.py
@@ -1,10 +1,11 @@
 # I don't know how to test this yet
-import json
+from os.path import exists
 
-from config import ControllerConfig
-
+import pytest
 import zmq as zmq
 from zmq.asyncio import Context, Poller
+
+from config import ControllerConfig
 
 
 class ZmqReceiver:
@@ -18,29 +19,43 @@ class ZmqReceiver:
         self.poller = Poller()
         self.poller.register(self.pull, zmq.POLLIN)
 
-
-    async def zmq_receiver(self, ctx: Context, url: str) -> None:
-        """receive messages with polling"""
-        while True:
-            events = await poller.poll()
-            if pull in dict(events):
-                msg = await pull.recv_multipart()
-                dict_received = json.loads(msg[0].decode("utf-8"))
-                print('received via zmq', dict_received)
-
-                try:
-                    target_temp = dict_received["target-temp"]
-                    await write_serial_string(writer, target_temp)
-                except KeyError as err_info:
-                    print(f"{err_info} not received" )
-
+    # async def zmq_receiver(self, ctx: Context, url: str) -> None:
+    #     """receive messages with polling"""
+    #     while True:
+    #         events = await poller.poll()
+    #         if pull in dict(events):
+    #             msg = await pull.recv_multipart()
+    #             dict_received = json.loads(msg[0].decode("utf-8"))
+    #             print('received via zmq', dict_received)
+    #
+    #             try:
+    #                 target_temp = dict_received["target-temp"]
+    #                 await write_serial_string(writer, target_temp)
+    #             except KeyError as err_info:
+    #                 print(f"{err_info} not received")
 
 
-def test_create_zmq_receiver():
+@pytest.fixture
+def valid_config_file():
+    filename = "./test_valid_config_file.txt"
+    assert exists(filename)
+    return filename
 
+
+def test_create_zmq_receiver(valid_config_file):
     config = ControllerConfig("./test_valid_config_file.txt")
-
     receiver = ZmqReceiver(config)
-
     assert receiver
 
+
+@pytest.fixture
+def receiver(valid_config_file):
+    config = ControllerConfig(valid_config_file)
+    receiver = ZmqReceiver(config)
+    assert receiver
+    return receiver
+
+
+@pytest.mark.asyncio
+async def test_receive_json(receiver):
+    assert False
