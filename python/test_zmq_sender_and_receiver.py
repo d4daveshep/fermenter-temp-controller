fda7197ddc2450e0316dba173e6e6e439ac8d285
d4daveshep <david@daveshep.net.nz>
2022-10-27 20:49:25 +1300

    merged tests for zmq sender and receiver to single file tested round trip

--- python/test_zmq_sender_and_receiver.py
+++ python/test_zmq_sender_and_receiver.py
@@ -1,3 +1,4 @@
+# I don't know how to test this yet
 import json
 import random
 from os.path import exists
@@ -5,6 +6,7 @@ from os.path import exists
 import pytest
 
 from config import ControllerConfig
+from zmq_receiver import ZmqReceiver
 from zmq_sender import ZmqSender
 
 
@@ -13,6 +15,20 @@ def valid_config_file():
     filename = "./test_valid_config_file.txt"
     assert exists(filename)
     return filename
+
+
+@pytest.fixture
+def receiver(valid_config_file):
+    config = ControllerConfig(valid_config_file)
+    receiver = ZmqReceiver(config)
+    assert receiver
+    return receiver
+
+
+def test_create_zmq_receiver(valid_config_file):
+    config = ControllerConfig("./test_valid_config_file.txt")
+    receiver = ZmqReceiver(config)
+    assert receiver
 
 
 @pytest.fixture
@@ -35,12 +51,19 @@ def test_create_zmq_sender(valid_config_file):
         assert False
 
 
-
 @pytest.mark.asyncio
-async def test_send_json(sender):
+async def test_send_and_receive_json(valid_config_file, receiver):
+    config = ControllerConfig(valid_config_file)
+
     target_temp = random.randrange(10, 30)
     string_to_send = json.dumps({"target-temp": target_temp})
     try:
+        sender = ZmqSender(config)
         await sender.send_string(string_to_send)
+
+        message_received = await receiver.wait_for_a_message()
+        string_received = (message_received[0]).decode("utf-8")
+        assert string_to_send == string_received
+
     except Exception as err_info:
         assert False
