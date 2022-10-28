9c39f13cdaa8dfd737506ff22f8f0e3736de25ea
d4daveshep <david@daveshep.net.nz>
2022-10-29 08:39:10 +1300

    more file moving to tidy up the directories

--- build_run/build_and_run_web_docker_image.sh
+++ build_run/build_and_run_web_docker_image.sh
@@ -3,7 +3,7 @@
 IMAGE_NAME="web_apis"
 
 # Docker build
-docker build -f ./Dockerfile.fastapi -t ${IMAGE_NAME} .
+docker build -f ./Dockerfile.web_apis -t ${IMAGE_NAME} .
 
 # Docker run
 docker run --network="host" ${IMAGE_NAME}
