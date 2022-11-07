c36bea3ddae7ce7d2014824387440ea05b065171
d4daveshep <david@daveshep.net.nz>
2022-11-07 19:59:25 +1300

    added get_last_record method - need to add time field still

--- tests/test_database.py
+++ tests/test_database.py
@@ -59,19 +59,61 @@ def test_create_point_object(temperature_database):
     assert point_1.__eq__(point_2)
 
 
-def test_write_record_to_database(temperature_database):
+def test_write_record_to_database_1(temperature_database):
+    timestamp = datetime.utcnow()
     point = temperature_database.create_point(fermenter_temp=21.3, ambient_temp=15.6, target_temp=20.0,
-                                              timestamp=datetime.utcnow())
-
+                                              timestamp=timestamp)
     temperature_database.write_temperature_record(point)
 
     # verify record
-    query_string = 'from(bucket: "temp-test") |> range(start: -1m)'
+    query_string = 'from(bucket: "temp-test") |> range(start: -1h) |> last()'
     client = temperature_database.get_database_client()
     query_api = client.query_api()
 
-    tables = query_api.query(query_string)
+    tablelist = query_api.query(query_string)
 
+    assert tablelist[0].records[0].values["_field"] == "ambient"
+    assert tablelist[0].records[0].values["_value"] == 15.6
+    assert tablelist[0].records[0].values["_time"].second == timestamp.second
+
+    assert tablelist[1].records[0].values["_field"] == "fermenter"
+    assert tablelist[1].records[0].values["_value"] == 21.3
+
+    assert tablelist[2].records[0].values["_field"] == "target"
+    assert tablelist[2].records[0].values["_value"] == 20.0
+
+    pass
+
+
+def test_get_last_database_record(temperature_database):
+    timestamp = datetime.utcnow()
+    point = temperature_database.create_point(fermenter_temp=21.3, ambient_temp=15.6, target_temp=20.0,
+                                              timestamp=timestamp)
+    temperature_database.write_temperature_record(point)
+
+    results_dict = temperature_database.get_last_record()
+
+    assert results_dict["fermenter"] == 21.3
+    assert results_dict["target"] == 20.0
+    assert results_dict["ambient"] == 15.6
+    assert results_dict["time"].second == timestamp.second
+
+    pass
+
+
+# def test_write_record_to_database(temperature_database):
+#     point = temperature_database.create_point(fermenter_temp=21.3, ambient_temp=15.6, target_temp=20.0,
+#                                               timestamp=datetime.utcnow())
+#
+#     temperature_database.write_temperature_record(point)
+#
+#     # verify record
+#     query_string = 'from(bucket: "temp-test") |> range(start: -1m)'
+#     client = temperature_database.get_database_client()
+#     query_api = client.query_api()
+#
+#     tables = query_api.query(query_string)
+#
 
 def test_write_record_to_database_from_fermenter_json(temperature_database):
     real_json_string = """{
