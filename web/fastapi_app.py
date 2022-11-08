7600c5f698574b56c4c4d274f8e2666de1cb4dcd
d4daveshep <david@daveshep.net.nz>
2022-11-08 18:33:10 +1300

    formatting

--- web/fastapi_app.py
+++ web/fastapi_app.py
@@ -10,11 +10,11 @@ app = FastAPI()
 config = ControllerConfig("config-test.ini")
 temperature_database = TemperatureDatabase(config)
 
+
 @app.get("/")
 def read_root():
     results_dict = temperature_database.get_last_record()
     return results_dict
-    # return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
