ea1d7f328deeb40ce69a67f010f50082629bf31d
d4daveshep <david@daveshep.net.nz>
2025-05-18 16:37:41 +1200

    implement TemperatureReading using pydantic and test conversion from json

--- src/controller/temperature.py
+++ src/controller/temperature.py
@@ -1,2 +1,27 @@
-class TemperatureReading:
-    pass
+from enum import StrEnum
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class ControllerAction(StrEnum):
+    NO_ACTION = "No Action"
+    REST = "Rest"
+    HEAT = "Heat"
+    COOL = "Cool"
+    ACTION_ERROR = "Error"
+
+
+class TemperatureReading(BaseModel):
+    instant: float
+    average: float
+    min: float
+    max: float
+    target: float
+    ambient: float
+    action: ControllerAction
+    rest: Optional[bool] = None
+    heat: Optional[bool] = None
+    cool: Optional[bool] = None
+    reason_code: str = Field(alias="reason-code")
+    json_size: int = Field(alias="json-size")
---
+++ tests/test_temperature.py
@@ -0,0 +1,39 @@
+from controller.temperature import TemperatureReading, ControllerAction
+import json
+import pytest
+
+
+@pytest.fixture
+def json_string() -> str:
+    return """{
+        "instant" : 21,
+        "average" : 21.3,
+        "min" : 18.9,
+        "max" : 23.4,
+        "target" : 20,
+        "ambient" : 12.3,
+        "action" : "Cool",
+        "cool" : true,
+        "reason-code" : "RC5.1",
+        "json-size" : "123"
+    }"""
+
+
+def test_temperature_reading(json_string: str):
+    # json_dict: dict = json.loads(json_string)
+    # data_dict: dict = TemperatureReading.convert_dict_int_values_to_float(json_dict)
+
+    temp_reading: TemperatureReading = TemperatureReading.model_validate_json(
+        json_string
+    )
+
+    assert temp_reading.instant == 21.0
+    assert temp_reading.average == 21.3
+    assert temp_reading.min == 18.9
+    assert temp_reading.max == 23.4
+    assert temp_reading.target == 20.0
+    assert temp_reading.ambient == 12.3
+    assert temp_reading.action == ControllerAction.COOL
+    assert temp_reading.cool is True
+    assert temp_reading.reason_code == "RC5.1"
+    assert temp_reading.json_size == 123
