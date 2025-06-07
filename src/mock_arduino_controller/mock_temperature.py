2a8d223773827ec0e76d740e5609eeedca0adbba
d4daveshep <david@daveshep.net.nz>
2025-06-07 15:45:39 +1200

    add and test step function to use for ambient temperature

--- src/mock_arduino_controller/mock_temperature.py
+++ src/mock_arduino_controller/mock_temperature.py
@@ -11,3 +11,21 @@ def sine_wave_value(interval: int, min: float = 19.0, max: float = 21.0) -> floa
     angle: float = (2 * math.pi * (interval % intervals)) / intervals
     sine_value: float = math.sin(angle)
     return avg + sine_value
+
+
+def step_value(
+    interval: int, low: float = 18.0, mid: float = 20.0, high: float = 22.0
+) -> float:
+    """
+    Return floats between low, mid and high using a step function at the specified interval.
+    """
+    intervals: int = 60
+
+    if interval % intervals < 20:
+        return low
+    elif interval % intervals < 40:
+        return mid
+    elif interval % intervals < 60:
+        return high
+    else:
+        raise ValueError(f"can't calculate step value for interval={interval}")
--- tests/unit/test_mock_serial_connection.py
+++ tests/unit/test_mock_serial_connection.py
@@ -1,5 +1,5 @@
 # test_mock_serial_connection.py
-from mock_arduino_controller.mock_temperature import sine_wave_value
+from mock_arduino_controller.mock_temperature import sine_wave_value, step_value
 
 
 def test_sine_wave():
@@ -19,3 +19,23 @@ def test_sine_wave():
 
     # check average value is mean of min and max
     assert total / 20.0 - (min_value + max_value) / 2.0 < 0.0001
+
+
+def test_step_value():
+    low_value: float = 18.0
+    mid_value: float = 20.0
+    high_value: float = 22.0
+    tolerance: float = 0.0001
+
+    for interval in range(60):
+        value = step_value(interval, low=low_value, mid=mid_value, high=high_value)
+        if interval % 60 < 20:
+            assert value - low_value < tolerance, f"interval:{interval}"
+        elif interval % 60 < 40:
+            assert value - mid_value < tolerance, f"interval:{interval}"
+        elif interval % 60 < 60:
+            assert value - high_value < tolerance, f"interval:{interval}"
+
+
+def test_arduino_output_generator():
+    assert False, "write this test"
