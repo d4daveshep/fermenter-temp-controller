751741e4171d8611640f6d1d705a47d8763fbbff
d4daveshep <david@daveshep.net.nz>
2025-06-11 14:54:27 +1200

    renamed and moved component test

--- tests/unit/test_mock_arduino_functions.py
+++ tests/unit/test_mock_arduino_functions.py
@@ -1,6 +1,3 @@
-import asyncio
-import serial_asyncio
-import pytest
 import json
 import math
 from typing import Any
@@ -88,33 +85,3 @@ def test_mock_arduino_temperature_reading_output():
 
     assert "json-size" in arduino_json
     assert arduino_json["json-size"] == 12345
-
-
-@pytest.mark.asyncio
-async def test_mock_arduino_output_generator(mock_serial_connection):
-    """
-    Test using the mock_serial_connection that returns as reader,writer tuple.
-    The reader returns output from the mock_arduino_output_generator on each readline() call.
-    By referencing the mock_serial_connection fixture we will get our patched serial connection.
-    """
-    # open serial connection (uses patched connection)
-    reader, writer = await serial_asyncio.open_serial_connection(
-        url="/dev/ttyUSB0",
-        baudrate=9600,
-    )
-
-    for _ in range(60):
-        try:
-            data: bytes = await reader.readline()
-            decoded_str: str = data.decode().strip()
-            if decoded_str:
-                arduino_json: dict[str, Any] = json.loads(decoded_str)
-                print(f"arduino output: {decoded_str}")
-                assert "json-size" in arduino_json
-                assert arduino_json["json-size"] == 12345
-                await asyncio.sleep(0.1)
-        except Exception as e:
-            print(f"Error reading from mock arduino serial connection: {e}")
-            await asyncio.sleep(0.5)
-
-    assert mock_serial_connection["reader"].readline.call_count == 60
