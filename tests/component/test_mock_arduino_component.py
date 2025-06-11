55d290d2af6dce493c72b82da7694aadbb01390d
d4daveshep <david@daveshep.net.nz>
2025-06-11 15:01:34 +1200

    tidy up tests and create placeholder for next test (failing)

--- tests/component/test_mock_arduino_component.py
+++ tests/component/test_mock_arduino_component.py
@@ -7,7 +7,7 @@ import serial_asyncio
 
 
 @pytest.mark.asyncio
-async def test_mock_arduino_output_generator(mock_serial_connection):
+async def test_mock_arduino_serial_output_generator(mock_serial_connection):
     """
     Test using the mock_serial_connection that returns as reader,writer tuple.
     The reader returns output from the mock_arduino_output_generator on each readline() call.
@@ -28,9 +28,18 @@ async def test_mock_arduino_output_generator(mock_serial_connection):
                 print(f"arduino output: {decoded_str}")
                 assert "json-size" in arduino_json
                 assert arduino_json["json-size"] == 12345
-                await asyncio.sleep(0.1)
+                # await asyncio.sleep(0.1)
         except Exception as e:
             print(f"Error reading from mock arduino serial connection: {e}")
-            await asyncio.sleep(0.5)
+            # await asyncio.sleep(0.5)
 
     assert mock_serial_connection["reader"].readline.call_count == 60
+
+
+@pytest.mark.asyncio
+async def test_mock_arduino_serial_write_target_temp(mock_serial_connection):
+    """
+    Test the mock serial connection can accept a target temperature and
+    update the target temperature returned on the next output
+    """
+    assert False, "implement this test"
