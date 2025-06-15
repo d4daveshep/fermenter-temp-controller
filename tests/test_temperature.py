8670fbf8b6fd6c663c8015f1b880e6bcad699933
d4daveshep <david@daveshep.net.nz>
2025-06-15 19:36:39 +1200

    test how to serialise TemperatureReading to json string and back'

--- tests/test_temperature.py
+++ tests/test_temperature.py
@@ -5,18 +5,20 @@ import pytest
 
 @pytest.fixture
 def json_string() -> str:
-    return """{
-        "instant" : 21,
-        "average" : 21.3,
-        "min" : 18.9,
-        "max" : 23.4,
-        "target" : 20,
-        "ambient" : 12.3,
-        "action" : "Cool",
-        "cool" : true,
-        "reason-code" : "RC5.1",
-        "json-size" : "123"
-    }"""
+    return '{\
+"instant":21.0,\
+"average":21.3,\
+"min":18.9,\
+"max":23.4,\
+"target":20.0,\
+"ambient":12.3,\
+"action":"Cool",\
+"rest":null,\
+"heat":null,\
+"cool":true,\
+"reason-code":"RC5.1",\
+"json-size":123\
+}'
 
 
 def test_temperature_reading(json_string: str):
@@ -34,3 +36,17 @@ def test_temperature_reading(json_string: str):
     assert temp_reading.cool is True
     assert temp_reading.reason_code == "RC5.1"
     assert temp_reading.json_size == 123
+
+
+def test_serialisation_of_temperature_reading(json_string: str):
+    temp_reading: TemperatureReading = TemperatureReading.model_validate_json(
+        json_string
+    )
+
+    serialised_string: str = temp_reading.model_dump_json(by_alias=True)
+    assert serialised_string == json_string
+
+    temp_reading_2: TemperatureReading = TemperatureReading.model_validate_json(
+        serialised_string
+    )
+    assert temp_reading == temp_reading_2
