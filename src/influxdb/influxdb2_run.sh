306edd339cc6ee163538b5cb8e2b0077a46e4027
d4daveshep <david@daveshep.net.nz>
2025-06-17 21:20:57 +1200

    update influxdb version to 2.7-alpine and move data directories

--- src/influxdb/influxdb2_run.sh
+++ src/influxdb/influxdb2_run.sh
@@ -4,8 +4,6 @@
 # image will stay running at port 8086
 
 docker run -d -p 8086:8086 \
-      -v $PWD/data:/var/lib/influxdb2 \
-      -v $PWD/config:/etc/influxdb2 \
-      influxdb:alpine
-      
-      
+  -v $PWD/influx_data/data:/var/lib/influxdb2 \
+  -v $PWD/influx_data/config:/etc/influxdb2 \
+  influxdb:2.7-alpine
